879eae4de7dc7e301a527f9af18870590f3e741f
Konstantinos Leimonis <k.leimonis@gmail.com>
2018-05-09 08:30:05 +0100

    Add custom typography depending on anonymous font

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,15 +1,13 @@
 import Typography from 'typography'
-import Wordpress2016 from 'typography-theme-wordpress-2016'
+import Anonymous from './dist/src/utils/typography-anonymous'
 
-Wordpress2016.overrideThemeStyles = () => ({
+Anonymous.overrideThemeStyles = () => ({
   'a.gatsby-resp-image-link': {
     boxShadow: 'none',
   },
 })
 
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(Anonymous)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
