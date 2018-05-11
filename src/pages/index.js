cd7b0e793ed390b057c6db8b4f01d761c03e0b90
Konstantinos Leimonis <k.leimonis@gmail.com>
2018-05-11 09:04:13 +0100

    Update blog posts list date display format

--- src/pages/index.js
+++ src/pages/index.js
@@ -68,7 +68,7 @@ export const pageQuery = graphql`
             slug
           }
           frontmatter {
-            date(formatString: "DD MMMM, YYYY")
+            date(formatString: "MMMM DD")
             title
           }
         }
